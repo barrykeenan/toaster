c0269897c672bb349f46687e40e109e852997f3e
Barry Keenan <barry.keenan@chrometoaster.com>
2021-09-13 23:25:25 +1200

    Update build process to copy index.html and assets/

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,41 @@
-const path = require('path')
+const path = require('path');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+// Set mode
+const argv = require('yargs').argv;
+const devMode = argv.mode !== 'production';
 
 module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'docs'),
-    filename: 'bundle.js',
-  },
-  performance: {
-    maxEntrypointSize: 1024000,
-    maxAssetSize: 1024000
-  },
-  devServer: {
-    static: './docs',
-    hot: true,
-    compress: true,
-    client: {
-        progress: true,
-        overlay: {
-          errors: true,
-          warnings: false,
+    mode: argv.mode,
+    devtool: devMode ? 'source-map' : false,
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/index.html', to: '.' },
+                { from: 'src/assets', to: './assets' }
+            ],
+        }),
+    ],
+    performance: {
+        maxEntrypointSize: 1024000,
+        maxAssetSize: 1024000,
+    },
+    devServer: {
+        static: './docs',
+        hot: true,
+        compress: true,
+        client: {
+            progress: true,
+            overlay: {
+                errors: true,
+                warnings: false,
+            },
         },
-      },
-  },
-}
+    },
+};
