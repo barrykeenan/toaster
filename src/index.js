f07ec8e91c048c336aeb2dec23ed887713d1ae5b
Barry Keenan <barry.keenan@chrometoaster.com>
2021-09-14 02:18:41 +1200

    Add WIP lighting

--- src/index.js
+++ src/index.js
@@ -8,18 +8,17 @@ import {
     TubeGeometry,
     MeshBasicMaterial,
     MeshStandardMaterial,
-    sRGBEncoding,
     TextureLoader,
     EquirectangularReflectionMapping,
-    WebGLCubeRenderTarget,
-    CubeTextureLoader,
     PMREMGenerator,
-    AmbientLight,
-    DirectionalLight,
     Raycaster,
     CatmullRomCurve3,
-    
+    HemisphereLight,
+    Color,
+    PointLight,
+    PointLightHelper
 } from 'three';
+
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import * as dat from 'three/examples/jsm/libs/dat.gui.module';
@@ -46,7 +45,10 @@ function init() {
 
     var step = 0;
     var controls;
+    const pickableMeshes = [];
+    const toaster = new Group();
 
+    var hemiLight = new HemisphereLight(0xB3A79F, 0x635543, 3);
     initLights();
 
     const textureLoader = new TextureLoader();
@@ -56,26 +58,23 @@ function init() {
         scene.environment = pmremGenerator.fromEquirectangular(tx).texture;
     });
     environmentMap.mapping = EquirectangularReflectionMapping;
-    // sRGBEncoding
 
-    scene.environment = environmentMap;
     // scene.background = environmentMap;
-
 
     var material = new MeshStandardMaterial({
         color: 0xffffff,
-        metalness: 0.9,
+        // metalness: 0,
+        // roughness: 1,
+        metalness: 0.8,
         roughness: 0,
+        envMapIntensity: 0.78
     });
-
+    
     var redLaserMat = new MeshBasicMaterial({color: 0xff0000, transparent: true, opacity: 0.6});
     
-    const pickableMeshes = [];
-    const toaster = new Group();
+    initGUI();
 
     initScene();
-
-    initGUI();
 
     // add the output of the renderer to the html element
     document.getElementById('webgl-output').appendChild(renderer.domElement);
@@ -103,7 +102,8 @@ function init() {
             mesh.name = "toaster-body";
             mesh.children.forEach(function (child) {
                 child.material = material;
-                // child.geometry.castShadow = true;
+                child.geometry.castShadow = true;
+                child.geometry.receiveShadow = true;
                 pickableMeshes.push(child);
             });
             toaster.add(mesh);
@@ -113,7 +113,8 @@ function init() {
             mesh.name = "toaster-lever";
             mesh.children.forEach(function (child) {
                 child.material = material;
-                // child.geometry.castShadow = true;
+                child.geometry.castShadow = true;
+                child.geometry.receiveShadow = true;
                 pickableMeshes.push(child);
             });
             toaster.add(mesh);
@@ -122,13 +123,47 @@ function init() {
 
     function initLights() {
         // Ambient
-        var ambienLight = new AmbientLight(0x353535);
-        scene.add(ambienLight);
+        hemiLight.position.set(0, 150, 0);
+        hemiLight.intensity = 0.4;
+        scene.add(hemiLight);
 
-        var dirLight = new DirectionalLight(0xffffff);
-        // dirLight.castShadow = true;
-        dirLight.position.set(50, 10, 0);
-        scene.add(dirLight);
+        // Points
+        const sphereSize = 2;
+
+        const pointLight = new PointLight( 0xffffd9, 50000 );
+        pointLight.position.set( 180, 100, 200 ); // scene origin is 1.5m off ground
+        pointLight.decay = 2;
+        pointLight.castShadow = true;
+        scene.add( pointLight );
+        // scene.add( new PointLightHelper( pointLight, sphereSize ) );
+
+        const pointLight2 = new PointLight( 0xffffd9, 50000 );
+        pointLight2.position.set( -180, 120, 220 ); // scene origin is 1.5m off ground
+        pointLight2.decay = 2;
+        pointLight2.castShadow = true;
+        scene.add( pointLight2 );
+        // scene.add( new PointLightHelper( pointLight2, sphereSize ) );
+
+        const pointLight3 = new PointLight( 0xffffd9, 20000 );
+        pointLight3.position.set( 190, 150, -250 );
+        pointLight3.decay = 2;
+        pointLight3.castShadow = true;
+        scene.add( pointLight3 );
+        // scene.add( new PointLightHelper( pointLight3, sphereSize ) );
+        
+        const pointLight4 = new PointLight( 0xffffd9, 20000 );
+        pointLight4.position.set( 30, 150, -400 );
+        pointLight4.decay = 2;
+        pointLight4.castShadow = true;
+        scene.add( pointLight4 );
+        // scene.add( new PointLightHelper( pointLight4, sphereSize ) );
+
+        const pointLight5 = new PointLight( 0xffffd9, 20000 );
+        pointLight5.position.set( -180, 150, -250 );
+        pointLight5.decay = 2;
+        pointLight5.castShadow = true;
+        scene.add( pointLight5 );
+        // scene.add( new PointLightHelper( pointLight5, sphereSize ) );
     }
 
     function initGUI() {
@@ -141,14 +176,19 @@ function init() {
             }
     
             this.showRay = false;
+
+            this.hemisphere = true;
+            this.color = 0xB3A79F;
+            this.groundColor = 0x635543;
+            this.intensity = 0.4;
         };
     
         const gui = new dat.GUI();
         // gui.add( camera.position , 'x', -50, 50 );
         // gui.add( camera.position , 'y', -50, 50 );
         // gui.add( camera.position , 'z', -100, 0 );
-        // gui.add(material, 'metalness', 0, 1, 0.01);
-        // gui.add(material, 'roughness', 0, 1, 0.01);
+        gui.add(material, 'metalness', 0, 1, 0.01);
+        gui.add(material, 'roughness', 0, 1, 0.01);
         // gui.add(controls, 'bouncingSpeed', 0, 0.5);
         // gui.add(controls, 'heightScale', 0, 3, 0.1);
         
@@ -160,6 +200,23 @@ function init() {
         debugFolder.add(mouse, 'x').step(0.01).name("mouse x").listen();
         debugFolder.add(mouse, 'y').step(0.01).name("mouse y").listen();
         // debugFolder.open();
+
+        gui.add(controls, 'hemisphere').onChange(function (e) {
+            if (!e) {
+                hemiLight.intensity = 0;
+            } else {
+                hemiLight.intensity = controls.intensity;
+            }
+        });
+        gui.addColor(controls, 'color').onChange(function (e) {
+            hemiLight.color = new Color(e);
+        });
+        gui.addColor(controls, 'groundColor').onChange(function (e) {
+            hemiLight.groundColor = new Color(e);
+        });
+        gui.add(controls, 'intensity', 0, 2, 0.1).onChange(function (e) {
+            hemiLight.intensity = e;
+        });
     }
 
     function render() {
