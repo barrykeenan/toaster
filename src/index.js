c14a9b0a0fd88c9e4e088be07461bb99ed091474
Barry Keenan <barry.keenan@chrometoaster.com>
2021-09-21 01:42:37 +1200

    Formatting

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import {
     HemisphereLight,
     Color,
     PointLight,
-    PointLightHelper
+    PointLightHelper,
 } from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
@@ -48,13 +48,13 @@ function init() {
     const pickableMeshes = [];
     const toaster = new Group();
 
-    var hemiLight = new HemisphereLight(0xB3A79F, 0x635543, 3);
+    var hemiLight = new HemisphereLight(0xb3a79f, 0x635543, 3);
     initLights();
 
     const textureLoader = new TextureLoader();
     const pmremGenerator = new PMREMGenerator(renderer);
 
-    const environmentMap = textureLoader.load('assets/textures/photosphere/CT-office.jpg', tx => {
+    const environmentMap = textureLoader.load('assets/textures/photosphere/CT-office.jpg', (tx) => {
         scene.environment = pmremGenerator.fromEquirectangular(tx).texture;
     });
     environmentMap.mapping = EquirectangularReflectionMapping;
@@ -67,11 +67,11 @@ function init() {
         // roughness: 1,
         metalness: 0.8,
         roughness: 0,
-        envMapIntensity: 0.78
+        envMapIntensity: 0.78,
     });
-    
-    var redLaserMat = new MeshBasicMaterial({color: 0xff0000, transparent: true, opacity: 0.6});
-    
+
+    var redLaserMat = new MeshBasicMaterial({ color: 0xff0000, transparent: true, opacity: 0.6 });
+
     initGUI();
 
     initScene();
@@ -80,14 +80,13 @@ function init() {
     document.getElementById('webgl-output').appendChild(renderer.domElement);
     render();
 
-
     function initScene() {
-        toaster.name = "toaster";
+        toaster.name = 'toaster';
         scene.add(toaster);
         // console.log('Added toaster group: ', toaster);
-    
+
         var loader = new OBJLoader();
-    
+
         // loader.load('assets/models/cube/cube1m.obj', function (mesh) {
         //     mesh.name = "cube-1m";
         //     mesh.children.forEach(function (child) {
@@ -97,9 +96,9 @@ function init() {
         //     mesh.scale.set(0.2,0.2,0.2)
         //     toaster.add(mesh);
         // });
-    
+
         loader.load('assets/models/smeg-toaster/smeg-toaster-body.obj', function (mesh) {
-            mesh.name = "toaster-body";
+            mesh.name = 'toaster-body';
             mesh.children.forEach(function (child) {
                 child.material = material;
                 child.geometry.castShadow = true;
@@ -108,9 +107,9 @@ function init() {
             });
             toaster.add(mesh);
         });
-    
+
         loader.load('assets/models/smeg-toaster/smeg-toaster-lever.obj', function (mesh) {
-            mesh.name = "toaster-lever";
+            mesh.name = 'toaster-lever';
             mesh.children.forEach(function (child) {
                 child.material = material;
                 child.geometry.castShadow = true;
@@ -130,59 +129,59 @@ function init() {
         // Points
         const sphereSize = 2;
 
-        const pointLight = new PointLight( 0xffffd9, 50000 );
-        pointLight.position.set( 180, 100, 200 ); // scene origin is 1.5m off ground
+        const pointLight = new PointLight(0xffffd9, 50000);
+        pointLight.position.set(180, 100, 200); // scene origin is 1.5m off ground
         pointLight.decay = 2;
         pointLight.castShadow = true;
-        scene.add( pointLight );
+        scene.add(pointLight);
         // scene.add( new PointLightHelper( pointLight, sphereSize ) );
 
-        const pointLight2 = new PointLight( 0xffffd9, 50000 );
-        pointLight2.position.set( -180, 120, 220 ); // scene origin is 1.5m off ground
+        const pointLight2 = new PointLight(0xffffd9, 50000);
+        pointLight2.position.set(-180, 120, 220); // scene origin is 1.5m off ground
         pointLight2.decay = 2;
         pointLight2.castShadow = true;
-        scene.add( pointLight2 );
+        scene.add(pointLight2);
         // scene.add( new PointLightHelper( pointLight2, sphereSize ) );
 
-        const pointLight3 = new PointLight( 0xffffd9, 20000 );
-        pointLight3.position.set( 190, 150, -250 );
+        const pointLight3 = new PointLight(0xffffd9, 20000);
+        pointLight3.position.set(190, 150, -250);
         pointLight3.decay = 2;
         pointLight3.castShadow = true;
-        scene.add( pointLight3 );
+        scene.add(pointLight3);
         // scene.add( new PointLightHelper( pointLight3, sphereSize ) );
-        
-        const pointLight4 = new PointLight( 0xffffd9, 20000 );
-        pointLight4.position.set( 30, 150, -400 );
+
+        const pointLight4 = new PointLight(0xffffd9, 20000);
+        pointLight4.position.set(30, 150, -400);
         pointLight4.decay = 2;
         pointLight4.castShadow = true;
-        scene.add( pointLight4 );
+        scene.add(pointLight4);
         // scene.add( new PointLightHelper( pointLight4, sphereSize ) );
 
-        const pointLight5 = new PointLight( 0xffffd9, 20000 );
-        pointLight5.position.set( -180, 150, -250 );
+        const pointLight5 = new PointLight(0xffffd9, 20000);
+        pointLight5.position.set(-180, 150, -250);
         pointLight5.decay = 2;
         pointLight5.castShadow = true;
-        scene.add( pointLight5 );
+        scene.add(pointLight5);
         // scene.add( new PointLightHelper( pointLight5, sphereSize ) );
     }
 
     function initGUI() {
-        controls = new function () {
+        controls = new (function () {
             this.heightScale = 0.2;
             this.bouncingSpeed = 0.04;
-    
+
             this.outputObjects = function () {
                 console.log(scene.children);
-            }
-    
+            };
+
             this.showRay = false;
 
             this.hemisphere = true;
-            this.color = 0xB3A79F;
+            this.color = 0xb3a79f;
             this.groundColor = 0x635543;
             this.intensity = 0.4;
-        };
-    
+        })();
+
         const gui = new dat.GUI();
         // gui.add( camera.position , 'x', -50, 50 );
         // gui.add( camera.position , 'y', -50, 50 );
@@ -191,14 +190,14 @@ function init() {
         gui.add(material, 'roughness', 0, 1, 0.01);
         // gui.add(controls, 'bouncingSpeed', 0, 0.5);
         // gui.add(controls, 'heightScale', 0, 3, 0.1);
-        
+
         const debugFolder = gui.addFolder('Debug');
         debugFolder.add(controls, 'outputObjects');
         debugFolder.add(controls, 'showRay').onChange(function (e) {
-            if (tube) scene.remove(tube)
+            if (tube) scene.remove(tube);
         });
-        debugFolder.add(mouse, 'x').step(0.01).name("mouse x").listen();
-        debugFolder.add(mouse, 'y').step(0.01).name("mouse y").listen();
+        debugFolder.add(mouse, 'x').step(0.01).name('mouse x').listen();
+        debugFolder.add(mouse, 'y').step(0.01).name('mouse y').listen();
         // debugFolder.open();
 
         gui.add(controls, 'hemisphere').onChange(function (e) {
@@ -224,16 +223,16 @@ function init() {
         stats.update();
         orbitControls.update(clock.getDelta());
 
-	    // update the picking ray with the camera and mouse position
-	    raycaster.setFromCamera( mouse, camera );
+        // update the picking ray with the camera and mouse position
+        raycaster.setFromCamera(mouse, camera);
 
         // detect objects intersecting the picking ray
-	    const intersects = raycaster.intersectObjects( pickableMeshes );
-    
+        const intersects = raycaster.intersectObjects(pickableMeshes);
+
         if (intersects.length > 0) {
             // offset source of line to just below camera
             var cameraPosition = camera.position.clone();
-            cameraPosition.y = cameraPosition.y - 0.2; 
+            cameraPosition.y = cameraPosition.y - 0.2;
 
             var points = [];
             points.push(cameraPosition);
@@ -244,7 +243,7 @@ function init() {
             if (tube) {
                 scene.remove(tube);
             }
-            
+
             if (controls.showRay) {
                 tube = new Mesh(tubeGeometry, redLaserMat);
                 scene.add(tube);
@@ -266,7 +265,11 @@ function init() {
     var tube;
 
     function onDocumentMouseDown(event) {
-        var vector = new Vector3((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1, 0.5);
+        var vector = new Vector3(
+            (event.clientX / window.innerWidth) * 2 - 1,
+            -(event.clientY / window.innerHeight) * 2 + 1,
+            0.5
+        );
         vector = vector.unproject(camera);
 
         var raycaster = new Raycaster(camera.position, vector.sub(camera.position).normalize());
@@ -280,10 +283,10 @@ function init() {
         }
     }
 
-    function onDocumentMouseMove( event ) {
+    function onDocumentMouseMove(event) {
         // update mouse position in normalized device coordinates (-1 to +1)
-        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
     }
 
     function onResize() {
